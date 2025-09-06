343b8a070b0cc6fcc9b2037ffa42e998929fcff1
YAMOVIES <ya753121988@gmail.com>
2025-09-06 19:16:07 +0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,7 +10,7 @@ from datetime import datetime, time
 import math # Added for pagination calculation
 
 # --- Environment Variables ---
-MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://mewayo8672:mewayo8672@cluster0.ozhvczp.mongodb.net/?retryWrites=true&w=majoritye=Cluster0")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://ya753121988_db_user:WhAXy8j6AXm8MmyF@cluster0.wnhpfeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 TMDB_API_KEY = os.environ.get("TMDB_API_KEY", "7dc544d9253bccc3cfecc1c677f69819")
 ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "HDFLIXMAX198")
 ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "HDFLIXMAX198")
